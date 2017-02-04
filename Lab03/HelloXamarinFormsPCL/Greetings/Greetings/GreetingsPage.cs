ce3543e4269aa92b1da2001eed31177f233034d5
Pablo Tôndolo de Vargas <pablotdvsm@gmail.com>
2017-02-04 15:48:28 -0200

    Acabei o #Lab03 da #MaratonaXmarin e conheço as opções para compartilhar código em Xamarin!

---
+++ Lab03/HelloXamarinFormsPCL/Greetings/Greetings/GreetingsPage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Greetings
+{
+    public class GreetingsPage : ContentPage
+    {
+        public GreetingsPage()
+        {
+            var MyLabel = new Label();
+            MyLabel.Text = "Greetings, Xamarin.Forms!";
+            this.Content = MyLabel;
+
+            //Device.OnPlatform(iOS:() =>
+            //{
+            //    Padding = new Thickness(0, 20, 0, 0);
+            //});
+
+            //MyLabel.HorizontalOptions = LayoutOptions.Center;
+            //MyLabel.VerticalOptions = LayoutOptions.Center;            MyLabel.HorizontalTextAlignment = TextAlignment.Center;
+            MyLabel.VerticalTextAlignment = TextAlignment.Center;
+        }
+    }
+}
---
+++ Lab03/HelloXamarinFormsSAP/HelloXamarinFormsSAP/App.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace HelloXamarinFormsSAP
+{
+    public class App : Application
+    {
+        public App()
+        {
+            // The root page of your application
+            MainPage = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.Center,
+                    Children = {
+                        new Label {
+                            HorizontalTextAlignment = TextAlignment.Center,
+#if __IOS__
+                            Text = "Welcome to iOS Xamarin Forms!"
+#elif __ANDROID__
+                            Text = "Welcome to Android Xamarin Forms!"
+#elif WINDOWS_UWP
+                            Text = "Welcome to Windows UWP Xamarin Forms!"
+#elif WINDOWS_APP
+                            Text = "Welcome to Windows Xamarin Forms!"
+#elif WINDOWS_PHONE_APP
+                            Text = "Welcome to Windows Phone Xamarin Forms!"
+#endif
+
+						}
+                    }
+                }
+            };
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}

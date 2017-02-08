4f8f911691281ca7dd58829c5f9bc429b0c9b190
Pablo Tôndolo de Vargas <pablotdvsm@gmail.com>
2017-02-08 17:25:50 -0200

    Módulo 2, Lições 1 e 2

---
+++ Modulo2/Licao01/MainActivity.cs
@@ -0,0 +1,38 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Licao01
+{
+    [Activity(Label = "Licao01", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.Main);
+
+            Button btnConverter = FindViewById<Button>(Resource.Id.btnConverter);
+            EditText txtDolares = FindViewById<EditText>(Resource.Id.txtDoalares);
+            EditText txtReais = FindViewById<EditText>(Resource.Id.txtReais);
+
+            double reais, dolares;
+
+            btnConverter.Click += delegate
+            {
+                try
+                {
+                    dolares = double.Parse(txtDolares.Text);
+                    reais = dolares * 3.25;
+                    txtReais.Text = reais.ToString();
+                }
+                catch (System.Exception ex)
+                {
+                    Toast.MakeText(this, ex.Message, ToastLength.Short).Show();
+                }
+            };
+        }
+    }
+}
+

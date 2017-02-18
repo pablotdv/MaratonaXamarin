02be4ed2787e9467858965fb9083ff9de9311f3e
Pablo Tôndolo de Vargas <pablotdvsm@gmail.com>
2017-02-18 16:07:21 -0200

    Módulo 3 - Lição 01

---
+++ Modulo3/Licao01/ViewController.designer.cs
@@ -0,0 +1,91 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace Licao01
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton btnCalcular { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField txtBanco { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField txtCaja { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField txtCapitalContable { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField txtCuentasporCobrar { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField txtPagoaCredito { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField txtPagoaProveedores { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField txtRenta { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (btnCalcular != null) {
+                btnCalcular.Dispose ();
+                btnCalcular = null;
+            }
+
+            if (txtBanco != null) {
+                txtBanco.Dispose ();
+                txtBanco = null;
+            }
+
+            if (txtCaja != null) {
+                txtCaja.Dispose ();
+                txtCaja = null;
+            }
+
+            if (txtCapitalContable != null) {
+                txtCapitalContable.Dispose ();
+                txtCapitalContable = null;
+            }
+
+            if (txtCuentasporCobrar != null) {
+                txtCuentasporCobrar.Dispose ();
+                txtCuentasporCobrar = null;
+            }
+
+            if (txtPagoaCredito != null) {
+                txtPagoaCredito.Dispose ();
+                txtPagoaCredito = null;
+            }
+
+            if (txtPagoaProveedores != null) {
+                txtPagoaProveedores.Dispose ();
+                txtPagoaProveedores = null;
+            }
+
+            if (txtRenta != null) {
+                txtRenta.Dispose ();
+                txtRenta = null;
+            }
+        }
+    }
+}

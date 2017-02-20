f5f716346737c2d2facc2e7cc203c143015a3fce
Pablo Tôndolo de Vargas <pablotdvsm@gmail.com>
2017-02-19 23:28:26 -0300

    Módulo 3 - Lição 03 (Eventos)

---
+++ Modulo3/Licao03.Eventos/ViewController.cs
@@ -0,0 +1,20 @@
+using System;
+
+using UIKit;
+
+namespace Licao03.Eventos
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }       
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+    }
+}

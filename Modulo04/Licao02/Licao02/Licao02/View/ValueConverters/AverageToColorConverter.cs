31164084784e1a4234243503cacb7596f6521de7
Pablo Tôndolo de Vargas <pablotdvsm@gmail.com>
2017-02-23 20:58:48 -0300

    Módulo 04 - Lição 03

---
+++ Modulo04/Licao02/Licao02/Licao02/View/ValueConverters/AverageToColorConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Licao02.View.ValueConverters
+{
+    public class AverageToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double average = (double)value;
+
+            return average < 60 ? Color.Red : Color.Blue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

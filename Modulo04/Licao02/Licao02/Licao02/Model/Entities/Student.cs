f28715f2e2f4c36704d9407514e9b190d0d6fde1
Pablo Tôndolo de Vargas <pablotdvsm@gmail.com>
2017-02-23 19:35:06 -0300

    Módulo 4 - Lição 02

---
+++ Modulo04/Licao02/Licao02/Licao02/Model/Entities/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Licao02.Model.Entities
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Group { get; set; }
+        public string StudentNumber { get; set; }
+        public double Average { get; set; }
+    }
+}
---
+++ Modulo04/Licao02/Licao02/Licao02/Model/Entities/StudentDirectory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Licao02.Model.Entities
+{
+    public class StudentDirectory
+    {
+        private ObservableCollection<Student> students = new ObservableCollection<Student>();
+        public ObservableCollection<Student> Students { get { return students; } set { students = value; } }
+    }
+}
